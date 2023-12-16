08b4b1ed940c5255d3c657555e01f92e4748c974
Kevin Tran <kevin.tran@parker.com>
2023-12-16 11:01:30 -0800

    remaps

--- lua/k/remap.lua
+++ lua/k/remap.lua
@@ -1,2 +1,23 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- not sure what this functionality was called in Sublime but it's amazing
+vim.keymap.set("v", "<C-S-J>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-S-K>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<C-S-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-S-Up>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set('n', '<leader>y', '\"+y')
+vim.keymap.set('v', '<leader>y', '\"+y')
+vim.keymap.set('n', '<leader>Y', '\"+Y')
+
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-R><C-w>\\>/<C-r><C-w><Left><Left><Left>")
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
