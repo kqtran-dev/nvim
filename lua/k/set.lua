2306d0f052793bf3f03c5974f84f0df165c90cfd
Kevin Tran <kevin.tran@parker.com>
2023-12-16 11:01:44 -0800

    remaps

---
+++ lua/k/set.lua
@@ -0,0 +1,16 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
