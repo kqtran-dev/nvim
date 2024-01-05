983a79d1211aef7cf84b91bda45e3c7963171ff0
Kevin Tran <kevin.tran@parker.com>
2024-01-04 22:49:15 -0800

    add cursor change

--- lua/k/set.lua
+++ lua/k/set.lua
@@ -25,4 +25,4 @@ vim.opt.cursorline = true
 vim.opt.list = true
 
 vim.opt.listchars = "tab:⇤–⇥,trail:·,extends:⇢,precedes:⇠,space:·"
-
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
