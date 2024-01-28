a6e5decc264105f2035d61e717693268586ab2d1
k <11863815+kqtran-dev@users.noreply.github.com>
2024-01-27 22:42:10 -0800

    remaps

--- lua/k/remap.lua
+++ lua/k/remap.lua
@@ -2,17 +2,22 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- not sure what this functionality was called in Sublime but it's amazing
+-- select visual block, ctrl + shift + arrow to move the block up and down
 vim.keymap.set("v", "<C-S-J>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-S-K>", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("v", "<C-S-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-S-Up>", ":m '<-2<CR>gv=gv")
 
+-- half page jumps - keep cursor centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- cursor centered for search terms
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- deletes highlighted text into void register so you can keep existing register
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set('n', '<leader>y', '\"+y')
 vim.keymap.set('v', '<leader>y', '\"+y')
--- lua/k/set.lua
+++ lua/k/set.lua
@@ -13,14 +13,23 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/vim/undodir"
+vim.opt.undofile = true
 
 -- search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
 vim.opt.ignorecase = true
 vim.opt.termguicolors = true
 
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 vim.opt.list = true
 
