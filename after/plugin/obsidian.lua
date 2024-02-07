66cac744658e5867722d0b50ae186eaba51fc9b1
Kevin Tran <kevin.tran@parker.com>
2024-02-06 21:06:21 -0800

    obsidian

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -2,9 +2,10 @@ local utils = require("core.utils")
 
 vim.opt.wrap = true
 
+vim.keymap.set('n', 'pf', 'builtin.ObsidianSearch<CR>', {noremap = true, silent = true})
+
 if utils.os_name == "Windows_NT" then
     vim.api.nvim_set_keymap('n', 'gf', ':ObsidianFollowLink<CR>', {noremap = true, silent = true})
 end
 
-
-vim.api.nvim_set_keymap('n', 'pf', ':ObsidianSearch<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>po', ':ObsidianSearch<CR>', {noremap = true, silent = true})
