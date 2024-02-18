bdfb79932097a3eac5aed2cf9b52124ed7211e96
Kevin Tran <kevin.tran@parker.com>
2024-02-17 21:09:05 -0800

    set

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -75,7 +75,7 @@ vim.api.nvim_create_autocmd({ "FocusLost" }, {
 vim.opt.conceallevel = 1
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-    pattern = "*.md",
+    pattern = "*",
     command = "write",
 })
 
