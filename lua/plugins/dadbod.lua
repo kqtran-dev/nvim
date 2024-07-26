9f93e7ccfbe364e6fee1814b0e697e9f410f690f
Kevin Tran <kevin.tran@parker.com>
2024-07-26 18:24:09 -0400

    dadbod

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,17 +1,22 @@
 return {
-  'kristijanhusak/vim-dadbod-ui',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
+    'tpope/vim-dadbod',
+    'pbogut/vim-dadbod-ssh',
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+        { 'tpope/vim-dadbod', lazy = true },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+        'DBUI',
+        'DBUIToggle',
+        'DBUIAddConnection',
+        'DBUIFindBuffer',
+    },
+    init = function()
+        -- Your DBUI configuration
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.dbs = {
+            flightdb = "mysql://metabase@k/flightdb",
+        }
+    end,
 }
