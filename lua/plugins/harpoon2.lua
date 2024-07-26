b86aff2c75aac0ce615bad6ade6fe6b9844fb392
Kevin Tran <kevin.tran@parker.com>
2024-07-26 16:46:39 -0400

    harppon

--- lua/plugins/harpoon2.lua
+++ lua/plugins/harpoon2.lua
@@ -1,21 +1,17 @@
 return {
-    "ThePrimeagen/harpoon",
+    "theprimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = true,
     config = function()
-        require("harpoon").setup()
+      require("harpoon"):setup()
     end,
     keys = {
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end),
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end),
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)                   ,
-        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)                   ,
-        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)                   ,
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)                   ,
-        -- Toggle previous & next buffers stored within Harpoon list                            ,
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)                    ,
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)                    ,
-    }
-
+      { "<leader>A", function() require("harpoon"):list():append() end, desc = "harpoon file", },
+      { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
+      { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
+      { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
+      { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
+      { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
+      { "<leader>5", function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
+    },
 }
