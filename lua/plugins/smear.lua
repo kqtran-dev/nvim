6f472e8c6948c0cac7bddad7d1dc13fe454f3c05
Kevin Q. Tran <kevin.tran@parker.com>
2024-11-27 08:04:57 -0800

    smear

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -21,5 +21,10 @@ return {
 
     -- Attempt to hide the real cursor when smearing.
     hide_target_hack = true,
+    stiffness = 0.8,               -- 0.6      [0, 1]
+    trailing_stiffness = 0.6,      -- 0.3      [0, 1]
+    trailing_exponent = 0,         -- 0.1      >= 0
+    distance_stop_animating = 0.5, -- 0.1      > 0
+    hide_target_hack = false,      -- true     boolean
   },
 }
