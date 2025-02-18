998cfcb245f7fc3624ea985fc1650ae049b3e0b8
k <k@ktmbp.localdomain>
2025-02-17 22:43:17 -0800

    blink

--- lua/plugins/lsp/copilot-cmp.lua
+++ lua/plugins/lsp/copilot-cmp.lua
@@ -1,11 +1,11 @@
 return {
-    'zbirenbaum/copilot-cmp',
-    event = { "InsertEnter", "LspAttach" },
-    config = function()
-    require("copilot_cmp").setup({
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-        }
-        )
-    end
+    -- 'zbirenbaum/copilot-cmp',
+    -- event = { "InsertEnter", "LspAttach" },
+    -- config = function()
+    -- require("copilot_cmp").setup({
+    --         suggestion = { enabled = false },
+    --         panel = { enabled = false },
+    --     }
+    --     )
+    -- end
 }
--- lua/plugins/lsp/copilot.lua
+++ lua/plugins/lsp/copilot.lua
@@ -1,9 +1,11 @@
 return {
-    'zbirenbaum/copilot.lua',
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
+  'zbirenbaum/copilot.lua',
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
     -- vim.g.copilot_node_command = "C:\\Program Files\\nodejs\\node"
-    require("copilot").setup({})
-    end,
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })    end,
 }
