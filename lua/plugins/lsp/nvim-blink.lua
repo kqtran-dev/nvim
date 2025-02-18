7c8d57f116b223896e9589d217eb1b80309da4cc
k <k@ktmbp.localdomain>
2025-02-17 23:36:01 -0800

    blink

--- lua/plugins/lsp/nvim-blink.lua
+++ lua/plugins/lsp/nvim-blink.lua
@@ -1,46 +1,32 @@
 return {
   'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  -- dependencies = 'rafamadriz/friendly-snippets',
   dependencies = {
-    'giuxtaposition/blink-cmp-copilot',
+    'fang2hou/blink-copilot',
     'rafamadriz/friendly-snippets',
   },
-
-  -- use a release tag to download pre-built binaries
   version = '*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
     completion = {
       ghost_text = {
         enabled = true
       },
       menu = {
         draw = {
-          treesitter = {
-            'lsp'
-          }
-        }
-      }
+          columns = {
+            { "kind_icon", }, { "label", "label_description", gap = 1, "kind" },
+          },
+          -- treesitter = {
+          --   'lsp'
+          -- },
+        },
+      },
     },
     keymap = {
       preset = 'super-tab',
     },
-
     appearance = {
       use_nvim_cmp_as_default = false,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono',
-      -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
       kind_icons = {
         Copilot = "",
         Text = '󰉿',
@@ -79,7 +65,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot'},
+      default = { 'lsp', 'path', 'snippets', 'cmdline', 'buffer', 'copilot'},
       providers = {
         copilot = {
           enabled = true,
@@ -93,10 +79,30 @@ return {
         }
       }
     },
+    cmdline = {
+      enabled = true,
+      keymap = nil, -- Inherits from top level `keymap` config when not set
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == '/' or type == '?' then return { 'buffer' } end
+        -- Commands
+        if type == ':' or type == '@' then return { 'cmdline' } end
+        return {}
+      end,
+      completion = {
+        trigger = {
+          show_on_blocked_trigger_characters = {},
+          show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
+        },
+        menu = {
+          auto_show = nil, -- Inherits from top level `completion.menu.auto_show` config when not set
+          draw = {
+            columns = { { 'label', 'label_description', gap = 1 } },
+          },
+        }
+      }
+    }
   },
-  --   appearance = {
-  --   },
-  -- opts_extend is supposed to be used to extend the default sources list
   opts_extend = { "sources.default" }
-  -- }
 }
