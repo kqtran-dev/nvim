e0134a4c320b1fd221b39fc2f9730bcb9c0fd9ff
k <k@ktmbp.localdomain>
2025-02-25 20:32:58 -0800

    marks

---
+++ lua/plugins/editing/marks-nvim.lua
@@ -0,0 +1,13 @@
+return {
+  "chentoast/marks.nvim",
+  event = "VeryLazy",
+  opts = {
+    bookmark_0 = {
+    sign = "⚑",
+    virt_text = "hello world",
+    -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
+    -- defaults to false.
+    annotate = false,
+  },
+  },
+}
