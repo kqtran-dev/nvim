c8ce0a6ffde12aa11959d8b0322f404770a2a225
Kevin Q. Tran <kevin.tran@parker.com>
2025-03-13 17:02:49 -0700

    remove cmdline

--- lua/plugins/editing/blink.lua
+++ lua/plugins/editing/blink.lua
@@ -83,7 +83,13 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'cmdline', 'buffer', 'copilot'},
+      default = { 
+        'lsp', 
+        'path', 
+        'snippets', 
+        -- 'cmdline', 
+        'buffer', 
+        'copilot'},
       providers = {
         copilot = {
           enabled = true,
