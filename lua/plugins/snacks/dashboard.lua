0edb050bc62fabaab224f18e45ae5ae5ba3a1c9f
Kevin Q. Tran <Kevin.Tran@parker.com>
2025-07-26 15:15:48 -0700

    dashboard

--- lua/plugins/snacks/dashboard.lua
+++ lua/plugins/snacks/dashboard.lua
@@ -1,41 +1,42 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-        dashboard = {
-                sections = {
-                    {
-                        pane = 1,
-                        section = "terminal",
-                        cmd = "pokemon-colorscripts -r 1-2",
-                        height = 20,
-                        padding = 1,
-                    },
-                    { section = "keys", gap = 1, padding = 1 },
-                    {
-                        pane = 2,
-                        icon = " ",
-                        title = "Recent Files",
-                        section = "recent_files",
-                        indent = 2,
-                        padding = 1,
-                    },
-                    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-                    {
-                        pane = 2,
-                        icon = " ",
-                        title = "Git Status",
-                        section = "terminal",
-                        enabled = function()
-                            return Snacks.git.get_root() ~= nil
-                        end,
-                        cmd = "git status --short --branch --renames",
-                        height = 5,
-                        padding = 1,
-                        ttl = 5 * 60,
-                        indent = 3,
-                    },
-                    { section = "startup" },
-                },
-            },
-    },
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			sections = {
+				-- {
+				--     pane = 1,
+				--     section = "terminal",
+				--     cmd = "Welcome",
+				--     height = 20,
+				--     padding = 1,
+				-- },
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Recent Files",
+					section = "recent_files",
+					indent = 2,
+					padding = 1,
+				},
+				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
+				{ section = "startup" },
+			},
+		},
+	},
 }
