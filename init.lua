cfeb6c7298e99959ec5cb6124b1de854ff74f8f5
Kevin Q. Tran <Kevin.Tran@parker.com>
2025-07-30 12:00:15 -0700

    update for ws

--- init.lua
+++ init.lua
@@ -18,4 +18,53 @@
 vim.g.skip_defaults_lua = 1
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
-require("core")
+if vim.uv.os_gethostname() == "BWF064UGW6YR14" then
+    require("core.globals")
+    require("core.remap")
+    require("core.utils")
+    require("core.set")
+    require("core.autocmds")
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath,
+        })
+    end
+
+    vim.opt.rtp:prepend(lazypath)
+
+    require("lazy").setup({
+        { import = "plugins.editing" }, -- UI plugins (themes, treesitter, devicons)
+        -- defaults = { lazy = true },
+        -- ui = {
+        --   border = "rounded"
+        -- },
+        -- performance = {
+        --   cache = {
+        --     enabled = true,
+        --   },
+        --   rtp = {
+        --     disabled_plugins = {
+        --       "editorconfig",
+        --       "matchit",
+        --       "matchparen",
+        --       "netrwPlugin",
+        --       "gzip",
+        --       "spellfile",
+        --       "tarPlugin",
+        --       "tohtml",
+        --       "tutor",
+        --       "zipPlugin",
+        --     },
+        --   },
+        -- },
+    })
+else
+    require("core")
+end
