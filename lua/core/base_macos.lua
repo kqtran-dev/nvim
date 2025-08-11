f2e5d1f57dcac558cd2cc2bf6a2e7b0ec261c856
k <k@Mac.localdomain>
2025-08-10 23:17:50 -0700

    Automated commit for updates

--- lua/core/base_macos.lua
+++ lua/core/base_macos.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
         { import = "plugins.snacks" },
         { import = "plugins.ui" },
         { import = "plugins.utils" },
+        { import = "plugins.db" },
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
