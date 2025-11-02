4b7774fb9455c1092ec8542496e3447386c922a5
k <k@Mac.socal.rr.com>
2025-11-02 08:50:12 -0800

    update yazi binds

--- lua/plugins/utils/yazi-nvim.lua
+++ lua/plugins/utils/yazi-nvim.lua
@@ -8,14 +8,14 @@ return {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     { 
-      "<leader>-",
+      "<leader>E",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
     {
       -- Open in the current working directory
-      "<leader>cw",
+      "<leader>e",
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
